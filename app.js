579b969708436008100a463efb8265a26f0d3795
Julia Lisec <julialisec12@gmail.com>
2020-09-07 05:52:52 +0200

    created function celciusToFahrenheitAndKelvin

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
 
+function celciusToFahrenheitAndKelvin() {
+  const cTemp = parseFloat(celciusInput.value);
+  const fTemp = (cTemp * (9/5)) + 32;
+  const kTemp = cTemp + 273.15;
+  fahrenheitInput.value = fTemp;
+  kelvinInput.value = kTemp;
+}
+
+celciusInput.addEventListener('input', celciusToFahrenheitAndKelvin);
